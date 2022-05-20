b72fe9ce18ad3bf826ab4956760e685ddb1bc38d
George Duarte <contato@georgeduarte.com.br>
2022-05-20 09:44:10 -0300

    Operação de CRUD Básico

--- TestAlfaAPI/Controllers/ValuesController.cs
+++ TestAlfaAPI/Controllers/ValuesController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+/*
 namespace TestAlfaAPI.Controllers
 {
     //[ApiConventionType(typeof(DefaultApiConventions))]
@@ -148,3 +148,4 @@ namespace TestAlfaAPI.Controllers
 
     }
 }
+ */
